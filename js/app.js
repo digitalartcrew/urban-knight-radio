3b7417280407cc43ae818458c70569a296ec08ce
digitalartcrew <digitalartcrew@gmail.com>
2017-01-23 13:01:14 -0800

    Updated images

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// 'use strict';
+// var app = angular.module('ukr', []);
+// app.controller('MainCtrl', ['$scope', function($scope){
+//   $scope.sources = [
+//     "{"text": "a"},
+//         {"text": "b"},
+//         {"text": "c"},
+//         {"text": "d"}"
+//   ];
+// }]);
